4a3cb6587b11a8b251362a87be15d89e18d11e94
Natali Shanshiashvili <125400710+shanshika@users.noreply.github.com>
2023-03-08 00:09:28 +0400

    Update Program.cs

--- MyTeraLesson/Program.cs
+++ MyTeraLesson/Program.cs
@@ -1,11 +1,41 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
   class HomeWork5
   {
     static void Main(string[] args)
     {
-       Console.WriteLine();
-       Console.Write("Enter a Number: ");
-       return Int32.Parse(Console.ReadLine());
+      startGame();
+    }
+    static void startGame()
+    {
+        Welcome();
+        tries();
+    }
+    static void Welcome()
+    {
+        Console.WriteLine("Hello, let's start...");
+        Console.WriteLine();
+        Console.WriteLine("Read the instructions please...");
+        Console.WriteLine();
+        Instruction();
+    }
+    static void Instruction()
+    {
+        Console.WriteLine("I have a hidden number for you ");
+        Console.WriteLine("This number is between 0 to 20 ");
+        Console.WriteLine("Your must guess this number ");
+        Console.WriteLine("You have 3 attempts ");
+    }
+    static int randomNumber()
+    {
+      return new Random().Next(0, 21);
+    }
+       static int enterNumb()
+       {
+        Console.WriteLine();
+        Console.Write("Enter a Number: ");
+        return Int32.Parse(Console.ReadLine());
        }
     static void tries()
     {
@@ -48,8 +78,5 @@
             return;
         }
         Console.ReadLine();
-
-
     }
-}
-
+  } 
